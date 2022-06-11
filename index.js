c3b724b7d55aedda7b11835e20a1dc67d2dc65c3
Fredrick <fredrick.lokidor@student.moringaschool.com>
2022-06-11 18:33:28 +0300

    commit tree 55dbf0ba8a3b846b5611e105f7e85a2a7273e947

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(driverNames, searchqueries){
+    return driverNames.filter(function(results){
+        return results.toLowerCase() === searchqueries.toLowerCase()
+    })
+}
+
+function fuzzyMatch(driverNames,testStr){
+    return driverNames.filter(function(results){
+        return results.toLowerCase().indexOf(testStr.toLowerCase()) === 0
+    })
+}
+
+function matchName(driverNames,searchqueries){
+    return driverNames.filter(function(result){
+        return result.name === searchqueries
+    })
+}
